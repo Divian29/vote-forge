66912567cd9983c780c998ef9361b8a6d8f39d66
EngrCharles042 <obicharles94@gmail.com>
2024-04-30 14:50:06 +0100

    swagger configuration completed

--- src/main/java/voteforge/infrastucture/documentations/SwaggerConfiguration.java
+++ src/main/java/voteforge/infrastucture/documentations/SwaggerConfiguration.java
@@ -1,4 +1,34 @@
 package voteforge.infrastucture.documentations;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("voteforge")
+                        .description("Documentation for VoteForge SpringBoot application REST APIs")
+                        .version("v1.0").contact(new Contact().name("VoterForge")
+                                .email("ng.voteforge@gmail.com").url("https://github.com/Divian29/vote-forge"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 }
