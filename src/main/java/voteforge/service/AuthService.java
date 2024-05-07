f5840537211c999c20a7474f7826671454f87ecc
Olivia <ikajiubachidiogo@gmail.com>
2024-05-07 23:35:08 +0100

    email created and classes updated

---
+++ src/main/java/voteforge/service/AuthService.java
@@ -0,0 +1,8 @@
+package voteforge.service;
+
+import org.springframework.http.ResponseEntity;
+import voteforge.payload.response.ApiResponse;
+
+public interface AuthService {
+    ResponseEntity<ApiResponse>
+}
